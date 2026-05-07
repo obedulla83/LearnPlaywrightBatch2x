e2feab1ad66405017efd6034aa2e271beb4e3c57
Obedulla83 <obedulla@gmail.com>
2026-05-07 14:46:30 -0500

    Initial commit of Playwright learning code

---
+++ chapter_01_Basics/01_Basics.js
@@ -0,0 +1,3 @@
+console.log("Hello The Testing Academy!");
+let x=10;
+console.log("the value of x is: ",x);
---
+++ chapter_01_Basics/03_JS_Commands.js
@@ -0,0 +1,11 @@
+console.log(process.platform);
+
+//MACOS - darwin
+//Windows - win32
+//Linux - linux
+
+console.log(process.arch);
+//x64 - 64 bit
+//ia32 - 32 bit
+
+console.log("Node Version:", process.version);
